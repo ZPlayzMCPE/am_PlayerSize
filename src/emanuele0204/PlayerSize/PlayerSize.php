46720cf9baf1705017324fa44f840560f673ceda
emanuele0204 <emanuelecimetta12@gmail.com>
2017-01-10 19:45:26 +0100

    Create PlayerSize.php

---
+++ src/emanuele0204/PlayerSize/PlayerSize.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace emanuele0204\PlayerSize;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\entity\Entity;
+use pocketmine\{Server, Player};
+
+class PlayerSize extends PluginBase{
+    
+    public $b = array();
+    public function onEnable(){
+        $this->getLogger()->info("§8» §ePlayerSize attivo");
+        $this->getServer()->getCommandMap()->register("size", new pSize($this));
+    }
+    
+    public function respawn(PlayerRespawnEvent $e){
+        $o = $e->getPlayer();
+        if(!empty($this->b[$o->getName()])){
+            $nomep = $this->b[$o->getName()];
+            $o->setDataProperty(Entity::DATA_SCALE, Entity::DATA_TYPE_FLOAT, $nomep);
+        }
+    }
+}
+
+class pSize extends Command{
+    
+    private $p;
+    public function __construct($plugin){
+        $this->p = $plugin;
+        parent::__construct("size", "PlayerSize by emanuele0204");
+    }
+    
+    public function execute(CommandSender $g, $label, array $args){
+        if($g->hasPermission("emanuele0204.size")){
+            if(isset($args[0])){
+                if(is_numeric($args[0]) && $args < 20){
+                    $this->p->b[$g->getName()] = $args[0];
+                    $g->setDataProperty(Entity::DATA_SCALE, Entity::DATA_TYPE_FLOAT, $args[0]);
+                    $g->sendMessage("§8» §fSize cambiata in §e".$args[0]." §f!");
+                }elseif($args[0] == "reset"){
+                    if(!empty($this->p->b[$g->getName()])){
+                        unset($this->p->b[$g->getName()]);
+                        $g->setDataProperty(Entity::DATA_SCALE, Entity::DATA_TYPE_FLOAT, 1);
+                        $g->sendMessage("§8» §aSize ritornata normale!");
+                    }else{
+                        $g->sendMessage("§8» §cSize non valida");
+                    }
+                }else{
+                        $g->sendMessage("§8» §c/size §a1-20 §7=> §fImposta una size");
+                        $g->sendMessage("§8» §c/size §areset §7=> §fFai tornare normale la tua size");
+                        $g->sendMessage("§8» §c/size §ahelp §7=> §fVisualizza questo menù");
+                }
+            }
+          }
+        }
+      }

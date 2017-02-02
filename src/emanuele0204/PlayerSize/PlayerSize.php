1cb35004f4e06ac7d74fe3fe9f586ebc7bf52f2a
emanuele0204 <emanuelecimetta12@gmail.com>
2017-02-02 15:30:38 +0100

    Update PlayerSize.php

--- src/emanuele0204/PlayerSize/PlayerSize.php
+++ src/emanuele0204/PlayerSize/PlayerSize.php
@@ -3,41 +3,56 @@
 namespace emanuele0204\PlayerSize;
 
 use pocketmine\plugin\PluginBase;
-use pocketmine\command\{
-    Command, CommandSender
-};
-use pocketmine\{
-    event\Listener, event\player\PlayerRespawnEvent, Server, Player
-};
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\entity\Entity;
+use pocketmine\{Server, Player};
 
-class PlayerSize extends PluginBase implements Listener {
-
-    /** @var int[] $playerSizes */
-    public $playerSizes = array();
-
-    public function onEnable() {
-        //$this->getLogger()->info("§8» §ePlayerSize attivo"); odiosi messaggi che spammano sulla console
-        $this->getServer()->getCommandMap()->register("size", new SizeCommand($this));
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+class PlayerSize extends PluginBase{
+    
+    public $b = array();
+    public function onEnable(){
+        $this->getLogger()->info("§8» §ePlayerSize attivo");
+        $this->getServer()->getCommandMap()->register("size", new pSize($this));
     }
-
-    public function respawn(PlayerRespawnEvent $event) {
-        $player = $event->getPlayer();
-        $name = $player->getName();
-        if (isset($this->getPlayerSizes()[$name]))
-            $player->setDataProperty($player::DATA_SCALE, $player::DATA_TYPE_FLOAT, $this->getPlayerSizes()[$name]);
+    
+    public function respawn(PlayerRespawnEvent $e){
+        $o = $e->getPlayer();
+        if(!empty($this->b[$o->getName()])){
+            $nomep = $this->b[$o->getName()];
+            $o->setDataProperty(Entity::DATA_SCALE, Entity::DATA_TYPE_FLOAT, $nomep);
+        }
     }
+}
 
-    public function setSize(float $size, Player $player) {
-        $player->setDataProperty($player::DATA_SCALE, $player::DATA_TYPE_FLOAT, $size);
-        if ($size != 1)
-            $this->getPlayerSizes()[$player->getName()] = $size;
+class pSize extends Command{
+    
+    private $p;
+    public function __construct($plugin){
+        $this->p = $plugin;
+        parent::__construct("size", "PlayerSize by emanuele0204");
     }
-
-    /**
-     * @return \int[]
-     */
-    public function getPlayerSizes(): array {
-        return $this->playerSizes;
-    }
+    
+    public function execute(CommandSender $g, $label, array $args){
+        if($g->hasPermission("playersize.size")){
+            if(isset($args[0])){
+                if(is_numeric($args[0])){
+                  if ($args[0] >= 0 && $args[0] <= 20) {
+                    $this->p->b[$g->getName()] = $args[0];
+                    $g->setDataProperty(Entity::DATA_SCALE, Entity::DATA_TYPE_FLOAT, $args[0]);
+                    $g->sendMessage("§8» §fSize cambiata in §e".$args[0]." §f!");
+                }elseif($args[0] == "reset"){
+                    if(!empty($this->p->b[$g->getName()])){
+                        unset($this->p->b[$g->getName()]);
+                        $g->setDataProperty(Entity::DATA_SCALE, Entity::DATA_TYPE_FLOAT, 1);
+                        $g->sendMessage("§8» §aGrandezza ritornata normale!");
+                    }else{
+                        $g->sendMessage("§8» §cUsa §f/size §ereset §coppure §f/size §e<size>");
+                    }
+                }else{
+                    $g->sendMessage("§8» §eLa grandezza deve essere tra f0 §ee §f20 ");
+               }
+            }
+         }
+      }
+   }
 }
